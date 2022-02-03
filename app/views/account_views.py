18372c276ba4690a6d5834e1b3ce37848d26648f
Patrik Horsky <patrik.horsky@kcl.ac.uk>
2022-02-03 12:57:29 +0000

    Implement change password

--- app/views/account_views.py
+++ app/views/account_views.py
@@ -2,8 +2,37 @@ from django.conf import settings
 from django.views.generic.edit import FormView
 from django.contrib.auth import login
 from .mixins import LoginProhibitedMixin
-from app.forms import SignUpForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from app.forms import SignUpForm, PasswordForm
 from django.urls import reverse
+
+# View modified from Clucker
+class PasswordView(LoginRequiredMixin, FormView):
+    """View that handles password change requests."""
+
+    template_name = 'password.html'
+    form_class = PasswordForm
+
+    def get_form_kwargs(self, **kwargs):
+        """Pass the current user to the password change form."""
+
+        kwargs = super().get_form_kwargs(**kwargs)
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+    def form_valid(self, form):
+        """Handle valid form by saving the new password."""
+
+        form.save()
+        login(self.request, self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """Redirect the user after successful password change."""
+
+        # messages.add_message(self.request, messages.SUCCESS, "Password updated!")
+        return reverse('dummy')
+
 
 # View modified from Clucker
 class SignUpView(LoginProhibitedMixin, FormView):
--- bookclub/urls.py
+++ bookclub/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     path('log_in/', views.LogInView.as_view(), name='log_in'),
     path('log_out/', views.log_out, name='log_out'),
+    path('password/', views.PasswordView.as_view(), name='password'),
     path('dummy/', views.dummy, name='dummy'),
 ]
