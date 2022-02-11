95dfb0aea1c289962fc4b5b92117ae0e1228aadd
Patrik Horsky <patrik.horsky@kcl.ac.uk>
2022-02-11 11:28:54 +0000

    Change back to synchronous messaging

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', views.home, name='home'),
     path('', include('frontend.urls')),
-    path('', include('app.urls')),
+    path('dm', include('app.urls')),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     path('log_in/', views.LogInView.as_view(), name='log_in'),
     path('log_out/', views.log_out, name='log_out'),
