63e4add1d5b009ad847c8e794a06962ee4f339f9
Valentin <valentin.magis@hotmail.de>
2022-02-15 16:54:32 +0000

    Edit urls to reach sign up through react

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     # path('', views.home, name='home'),
     path('', include('frontend.urls')),
 
-    path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
+    # path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     # path('log_in/', views.LogInView.as_view(), name='log_in'),
     path('log_out/', views.log_out, name='log_out'),
     path('password/', views.PasswordView.as_view(), name='password'),
