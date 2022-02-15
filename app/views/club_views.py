06c945c78ded6b40877335b4ee5bbabed8b86e7e
Duncan Lau <k2038819@kcl.ac.uk>
2022-02-15 04:46:37 +0000

    [Club management - View tests]: Add tests for club list, user club list, accept applicant, reject applicant, ban member

--- app/views/club_views.py
+++ app/views/club_views.py
@@ -32,11 +32,10 @@ def user_club_list(request):
     clubs = Club.objects.filter(Q(owner=current_user) | Q(admins=current_user) | Q(members=current_user))
     return render(request, 'club_list.html', {'clubs': clubs})
 
-# TODO: refactor with sessions for club_id
 @login_required
 def accept_applicant(request, club_id, applicant_id):
     current_user = request.user
-    club = Club.objects.get(club_id=club_id)
+    club = Club.objects.get(pk=club_id)
     if club.owner == current_user:
         applicant = User.objects.get(pk=applicant_id)
         club.remove_applicant(applicant)
@@ -46,16 +45,16 @@ def accept_applicant(request, club_id, applicant_id):
 @login_required
 def reject_applicant(request, club_id, applicant_id):
     current_user = request.user
-    club = Club.objects.get(club_id=club_id)
+    club = Club.objects.get(pk=club_id)
     if club.owner == current_user:
         applicant = User.objects.get(pk=applicant_id)
         club.remove_applicant(applicant)
     return render(request, "dummy.html")
 
 @login_required
-def ban_members(request, club_id, member_id):
+def ban_member(request, club_id, member_id):
     current_user = request.user
-    club = Club.objects.get(club_id=club_id)
+    club = Club.objects.get(pk=club_id)
     if club.owner == current_user:
         member = User.objects.get(pk=member_id)
         club.remove_member(member)
