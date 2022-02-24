c2c043404a994ab602b195e5fb88488325b12477
Yonathan Setiawan <yonathanbali@gmail.com>
2022-02-24 18:48:26 +0800

    implement url for friends and friend requests api view

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from app.views.friend_views import FriendRequestsView, FriendsView
 from .views.account_views import CreateUser
 from .views.authentication_views import BlacklistTokenView
 
@@ -8,5 +10,7 @@ app_name = 'app'
 
 urlpatterns = [
     path('user/sign_up/', CreateUser.as_view(), name="create_user"),
-    path('user/log_out/blacklist/', BlacklistTokenView.as_view(), name='blacklist')
+    path('user/log_out/blacklist/', BlacklistTokenView.as_view(), name='blacklist'),
+    path('friends/', FriendsView.as_view(), name='friends'),
+    path('friend_requests/', FriendRequestsView.as_view(), name='friend_requests')
 ]
