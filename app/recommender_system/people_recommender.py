eb1b907ae48febff4efe15924cf2a9f748827413
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-02-25 12:37:44 +0000

    Implement recommending clubs to users

--- app/recommender_system/people_recommender.py
+++ app/recommender_system/people_recommender.py
@@ -5,7 +5,7 @@ import time
 from operator import itemgetter
 from random import shuffle
 
-from app.models import User
+from app.models import User, Club
 from app.recommender_system.books_recommender import get_top_n
 from app.recommender_system.genre_algo import get_isbns_for_a_genre
 
@@ -31,14 +31,7 @@ def get_top_n_users_by_favourite_books(uid, trainset, algo, n=10):
 
 
 def get_top_n_users_double_random(uid, trainset, algo, n=10):
-    """Get the top n users for a user using random users and random items
-
-    The number of books for which we compare the similarity between the users is
-    calculated as 100000 / the number of users, because I discovered that a 100000
-    comparisons takes a couple of seconds on my computer and we don't want to spend too
-    much time doing that
-
-    """
+    """Get the top n users for a user using random users and random items"""
 
     users = get_actual_users(trainset)
     if uid in users:
@@ -51,13 +44,7 @@ def get_top_n_users_double_random(uid, trainset, algo, n=10):
 
 
 def get_top_n_users_for_a_genre(uid, trainset, algo, genre, n=10):
-    """Get the top n users for a user using items from a given genre
-
-    The number of books for which we compare the similarity between the users is
-    calculated as 100000 / the number of users, because I discovered that a 100000
-    comparisons takes a couple of seconds on my computer and we don't want to spend too
-    much time doing that
-    """
+    """Get the top n users for a user using items from a given genre"""
 
     users = get_random_n_users(trainset, 100)
     if uid in users:
@@ -121,8 +108,8 @@ def get_actual_users(trainset):
     return list(set(all_trainset_users).intersection(database_users))
 
 
-def get_diff_for_list_of_users(uid1, uids, algo, items):
-    """Get the difference between a user and a list of users given a list of items"""
+def get_average_diff_for_list_of_users(uid1, uids, algo, items):
+    """Get the average difference between a user and a list of users given a list of items"""
 
     diff = 0
     for uid2 in uids:
@@ -130,13 +117,82 @@ def get_diff_for_list_of_users(uid1, uids, algo, items):
             pred1 = algo.predict(uid=uid1, iid=iid)
             pred2 = algo.predict(uid=uid2, iid=iid)
             diff += abs(pred1.est - pred2.est)
-    return diff
+    return diff / (len(items))
+
+
+def get_all_related_users(club):
+    users = list(club.members.all())
+    users.extend(club.admins.all())
+    users.append(club.owner)
+    return users
+
+
+def get_top_n_clubs_using_random_items(uid, algo, trainset, clubs, n=10):
+    """Get the top n clubs for a user using random items"""
+
+    num_of_users = 0
+    for club in clubs:
+        num_of_users += len(get_all_related_users(club))
+
+    items = get_random_n_items(trainset, int(100000 / num_of_users))
+
+    return get_top_n_clubs_for_items(algo, clubs, items, n, uid)
+
+
+def get_top_n_clubs_using_top_items_for_a_user(uid, algo, trainset, clubs, n=10):
+    """Get the top n clubs for a user using user's top books"""
+
+    num_of_users = 0
+    for club in clubs:
+        num_of_users += len(get_all_related_users(club))
+
+    max_number_of_items = int(100000 / num_of_users)
+    items = list(x[0] for x in get_top_n(uid, trainset, algo, max_number_of_items))
+
+    return get_top_n_clubs_for_items(algo, clubs, items, n, uid)
+
+
+def get_top_n_clubs_for_a_genre(uid, algo, trainset, clubs, genre, n=10):
+    """Get the top n clubs for a user using random books from a genre"""
+
+    num_of_users = 0
+    for club in clubs:
+        num_of_users += len(get_all_related_users(club))
+
+    max_number_of_items = int(100000 / num_of_users)
+    all_items = get_isbns_for_a_genre(genre, trainset)
+    items = get_random_n_items_from_a_list(all_items, max_number_of_items)
+
+    return get_top_n_clubs_for_items(algo, clubs, items, n, uid)
+
+
+def get_top_n_clubs_using_clubs_books(uid, algo, clubs, n=10):
+    """Get the top n clubs for a user using the club's books"""
+
+    clubs_with_diffs = []
+    for club in clubs:
+        uids = list((user.id for user in get_all_related_users(club)))
+        items = list((book.ISBN for book in club.books.all()))
+        clubs_with_diffs.append((club.id, get_average_diff_for_list_of_users(uid, uids, algo, items)))
+    clubs_with_diffs.sort(key=itemgetter(1))
+    return clubs_with_diffs[0:n]
+
+
+def get_top_n_clubs_for_items(algo, clubs, items, n, uid):
+    """Get the top n clubs for a user for a list of items"""
+
+    clubs_with_diffs = []
+    for club in clubs:
+        uids = list((user.id for user in get_all_related_users(club)))
+        clubs_with_diffs.append((club.id, get_average_diff_for_list_of_users(uid, uids, algo, items)))
+    clubs_with_diffs.sort(key=itemgetter(1))
+    return clubs_with_diffs[0:n]
 
 
 def get_top_n_users_test(algo, trainset, genre='fiction'):
     """A test for getting top n users for a user
 
-    It runs the following methods
+    It runs the following functions
     get_top_n_users_by_favourite_books, get_top_n_users_double_random
 
     """
@@ -187,15 +243,82 @@ def get_top_n_users_test(algo, trainset, genre='fiction'):
         logging.debug(f'{user} with difference {diff}')
 
 
-# TODO test this functionality when we have clubs data
-def get_best_fitting_n_clubs(uid, algo, trainset, clubs, n=10):
-    """Get the top n clubs for a user"""
+def get_top_n_clubs_test(algo, trainset, genre):
+    """A test for getting top n clubs for a user
 
-    # For the purpose of this function I assume that clubs is a list of clubs which have ids and users
-    items = get_random_n_items(trainset=trainset, n=100000)
-    clubs_with_diffs = []
-    for club in clubs:
-        clubs_with_diffs.append((club.id, get_diff_for_list_of_users(uid, club.members, algo, items)))
+    It runs the following functions
+    get_top_n_clubs_using_random_items, get_top_n_clubs_using_top_items_for_a_user, get_top_n_clubs_for_a_genre
 
-    clubs_with_diffs.sort(key=itemgetter(1))
-    return clubs_with_diffs[0:n]
+    """
+
+    start = time.time()
+    top_n = get_top_n_clubs_using_random_items(uid=1276726, algo=algo, trainset=trainset,
+                                               clubs=list(Club.objects.all()))
+    end = time.time()
+    logging.debug(f'Finished getting top n clubs with random items in {end - start} seconds')
+
+    # NOTE: The item choice is random so I cannot write any tests for it
+
+    # Print the top 10 clubs
+    for (club, diff) in top_n:
+        logging.debug(f'{club} with difference {diff}')
+
+    start = time.time()
+    top_n = get_top_n_clubs_using_top_items_for_a_user(uid=1276726, algo=algo, trainset=trainset,
+                                                       clubs=list(Club.objects.all()))
+    end = time.time()
+    logging.debug(f'Finished getting top n clubs with top items in {end - start} seconds')
+
+    # NOTE: The assertions only work for one particular training, they might be incorrect if you retrain the model
+    top_n_actual = [(6, 3.505534454684893),
+                    (9, 4.143084212012058),
+                    (10, 7.717597523564549),
+                    (14, 8.926585442235737),
+                    (13, 9.659160285675167),
+                    (1, 11.5182567703274),
+                    (2, 14.328265973238327),
+                    (11, 15.595305319348453),
+                    (3, 15.756530867111913),
+                    (8, 16.746178006115013)]
+
+    assert top_n == top_n_actual
+
+    # Print the top 10 clubs
+    for (club, diff) in top_n:
+        logging.debug(f'{club} with difference {diff}')
+
+    start = time.time()
+    top_n = get_top_n_clubs_for_a_genre(uid=1276726, algo=algo, trainset=trainset,
+                                        clubs=list(Club.objects.all()), genre=genre)
+    end = time.time()
+    logging.debug(f'Finished getting top n clubs with random items for a given genre in {end - start} seconds')
+
+    # NOTE: The item choice is random so I cannot write any tests for it
+
+    # Print the top 10 clubs
+    for (club, diff) in top_n:
+        logging.debug(f'{club} with difference {diff}')
+
+    start = time.time()
+    top_n = get_top_n_clubs_using_clubs_books(uid=1276726, algo=algo,
+                                              clubs=list(Club.objects.all()))
+    end = time.time()
+    logging.debug(f'Finished getting top n clubs with club\'s items in {end - start} seconds')
+
+    # NOTE: The assertions only work for one particular training, they might be incorrect if you retrain the model
+    top_n_actual = [(6, 3.2331997259111755),
+                    (9, 3.4104444156455855),
+                    (10, 6.998403692160351),
+                    (14, 8.04901214823293),
+                    (13, 8.677910865192112),
+                    (1, 9.915907208303397),
+                    (2, 12.151868348701255),
+                    (3, 12.977574592245945),
+                    (11, 13.9265376783672),
+                    (8, 14.696792659494873)]
+
+    assert top_n == top_n_actual
+
+    # Print the top 10 clubs
+    for (club, diff) in top_n:
+        logging.debug(f'{club} with difference {diff}')
