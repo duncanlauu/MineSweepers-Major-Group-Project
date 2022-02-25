db2a8941c9d899cafb216878701c971fe00bb850
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-02-25 13:08:47 +0000

    Uncomment code

--- app/management/commands/unseed.py
+++ app/management/commands/unseed.py
@@ -5,5 +5,5 @@ from app.models import Book, User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        # Book.objects.all().delete()
+        Book.objects.all().delete()
         User.objects.filter(is_superuser=False, is_staff=False).delete()
