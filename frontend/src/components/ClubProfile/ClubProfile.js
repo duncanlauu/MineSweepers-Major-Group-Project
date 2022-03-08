0dc9413d890ee764c3afc8d44c5b1dbc6ad4b634
Vedeesh Bali <vedeeshbali12345@gmail.com>
2022-03-08 15:24:42 +0000

    static files in gitignore

--- frontend/src/components/ClubProfile/ClubProfile.js
+++ frontend/src/components/ClubProfile/ClubProfile.js
@@ -3,29 +3,54 @@ import { Container, Row, Col, Button } from 'reactstrap';
 import { BookProfile, ProfileContainer, ProfileHeader } from './ClubProfileElements';
 import Nav from '../Nav/Nav';
 import ClubProfileTabs from './ClubProfileTabs.js';
+import { useParams } from 'react-router';
+import axiosInstance from '../../axios';
+import axios from 'axios';
 
-const ClubProfile = () => {
-  return (
-    <div>
-        <Container fluid>
-            <Row style={{ marginBottom:"3rem" }}>
-                <Nav />
-            </Row>
-            <Row>
-                <Col />
-                <Col xs={6}>
-                    <ProfileContainer>
-                        <ProfileHeader>
-                            Lorem Ipsum
-                            <ClubProfileTabs />
-                        </ProfileHeader>
-                    </ProfileContainer>
-                </Col>
-                <Col />
-            </Row>
-        </Container>
-    </div>
-  )
+export default class ClubProfile extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            club: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users/1`)
+        .then(
+            response => {
+                console.log(response)
+                this.setState({
+                    club: response.data
+                });
+            }
+        )
+    }
+
+    render() {
+        return(
+            <div>
+                <Container fluid>
+                    <Row style={{ marginBottom:"3rem" }}>
+                        <Nav />
+                    </Row>
+                    <Row>
+                        <Col />
+                        <Col xs={6}>
+                            <ProfileContainer>
+                                <ProfileHeader>
+                                    {this.state.club.map(
+                                        club => {club.name}
+                                    )}
+                                    <ClubProfileTabs />
+                                </ProfileHeader>
+                            </ProfileContainer>
+                        </Col>
+                        <Col />
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
 }
-
-export default ClubProfile
