41165221bf4dcaa3849a227168e47c5635d43ce9
Yonathan Setiawan <yonathanbali@gmail.com>
2022-03-08 20:26:32 +0800

    add urls for feed, posts, comments, replies

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from app.views.feed_views import AllPostsView, FeedView, PostView
+from app.views.feed_views import AllCommentsView, AllPostsView, AllRepliesView, CommentView, FeedView, PostView, ReplyView
 from .views.account_views import CreateUser
 from .views.authentication_views import BlacklistTokenView
 
@@ -13,5 +13,9 @@ urlpatterns = [
     path('user/log_out/blacklist/', BlacklistTokenView.as_view(), name='blacklist'),
     path('feed/', FeedView.as_view(), name='feed'),
     path('posts/', AllPostsView.as_view(), name='all_posts'),
-    path('posts/<int:post_id>', PostView.as_view(), name='post')
+    path('posts/<int:post_id>', PostView.as_view(), name='post'),
+    path('posts/<int:post_id>/comments/', AllCommentsView.as_view(), name='all_comments'),
+    path('posts/<int:post_id>/comments/<int:comment_id>', CommentView.as_view(), name='comment'),
+    path('posts/<int:post_id>/comments/<int:comment_id>/replies/', AllRepliesView.as_view(), name='all_replies'),
+    path('posts/<int:post_id>/comments/<int:comment_id>/replies/<int:reply_id>', ReplyView.as_view(), name='reply')
 ]
