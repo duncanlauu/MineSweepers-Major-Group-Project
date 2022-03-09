0281b14cb9d35cd8b8020a96ede1f1e293f844b4
Vedeesh Bali <vedeeshbali12345@gmail.com>
2022-03-09 16:57:41 +0000

    Add list of clubs, faulty individual club profile

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -17,6 +17,7 @@ import ChatWrapper from "./Chat/ChatWrapper.js"
 import HomePage from "./HomePage/HomePage";
 import Notifications from "./Notifications/Notifications";
 import CreateClub from "./CreateClub/CreateClub";
+import ListOfClubs from "./ListOfClubs/ListOfClubs";
 
 export default class App extends Component {
   constructor(props) {
@@ -34,7 +35,7 @@ export default class App extends Component {
             <Route path='/log_in/' element={<Login />}></Route>
             <Route path='/log_out/' element={<Logout />}></Route>
             <Route path='/error/' element={<Error404 />}></Route>
-            <Route path='/club_profile/' element={<ClubProfile />}></Route>
+            <Route path='/club_profile/:id' element={<ClubProfile />}></Route>
             <Route path='/sign_up/' element={<SignUp />}></Route>
             <Route path='/create_club/' element={<CreateClub />}></Route>
             <Route path='/notifications/' element={<Notifications />}></Route>
@@ -43,6 +44,7 @@ export default class App extends Component {
             <Route path='/password_reset/' element={<PasswordReset />}></Route>
             <Route path='/password_reset_confirm/:uid/:token' element={<PasswordResetConfirm />} />
             <Route path="/chat/:chatID/" element={<ChatWrapper />}></Route>
+            <Route path="/all_clubs/" element={<ListOfClubs />}></Route>
           </Routes>
           {/* <Footer /> */}
         </React.StrictMode>
