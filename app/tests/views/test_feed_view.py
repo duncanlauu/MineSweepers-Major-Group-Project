12804fbf710c8da75a967342db0d9ad8b68a8c6b
Yonathan Setiawan <yonathanbali@gmail.com>
2022-03-15 01:18:26 +0800

    start feed api testing

--- app/tests/views/test_feed_view.py
+++ app/tests/views/test_feed_view.py
@@ -1,19 +1,26 @@
-from django.test import TestCase
+from rest_framework.test import APIClient, APITestCase
+from rest_framework.reverse import reverse
+from app.models import User
 
 
-
-class FeedAPIViewTestCase(TestCase):
+class FeedAPIViewTestCase(APITestCase):
     """Tests of the Feed related API views."""
 
     fixtures = ['app/tests/fixtures/default_user.json',
                 'app/tests/fixtures/other_users.json',
+                'app/tests/fixtures/default_post.json',
+                'app/tests/fixtures/other_posts.json'
                 'app/tests/fixtures/default_club.json',
-                'app/tests/fixtures/default_book.json']
+                'app/tests/fixtures/others_clubs.json',
+                'app/tests/fixtures/default_book.json',
+                'app/tests/fixtures/other_books.json'
+                ]
 
     def setUp(self):
-        self.club = Club.objects.get(name="Joe's Club")
-        self.applicant = User.objects.get(username='jamesdoe')
-        self.url = reverse('reject_applicant', kwargs={'club_id': self.club.id, 'applicant_id': self.applicant.id})
+        self.client = APIClient()
+        self.user = User.objects.get(pk=1)
+        self.client.force_authenticate(self.user)
 
-    def test_reject_applicant_url(self):
-        self.assertEqual(self.url, f'/reject_applicant/{self.club.id}/{self.applicant.id}')
+    def test_get_feed(self):
+        response = self.client.get(reverse('app:feed'))
+        
