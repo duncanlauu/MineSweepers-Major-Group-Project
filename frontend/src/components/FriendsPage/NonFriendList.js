330f908a63def9f60b52fdfc152892f2a8c4965d
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-03-15 16:00:54 +0000

    Fix the recommender stuff

--- frontend/src/components/FriendsPage/NonFriendList.js
+++ frontend/src/components/FriendsPage/NonFriendList.js
@@ -15,15 +15,17 @@ export default function NonFriendList(props) {
     useEffect(() => {
         //getRecommendedFriends();
         getAllNonFriends();
-    }, []);
+    }, [user]);
 
     const getAllNonFriends = () => {
+        console.log(user.id)
         axiosInstance
-            .get(`recommender/10/${user.id}/top_n_users_random_books/`)
+            .get(`recommender/10/9/top_n_users_random_books/`)
             .then((res) => {
-                console.log("Buenos")
-                // const allNonFriends = res.data.non_friends;
-                // setNonFriends(allNonFriends)
+                console.log("Buenos dias")
+                console.log(res.data)
+                const allNonFriends = res.data;
+                setNonFriends(allNonFriends)
             })
             .catch(error => console.error(error));
     }
@@ -57,6 +59,7 @@ export default function NonFriendList(props) {
             })
     }
 
+    // display recommended friends
     const displayNonFriends = (e) => {
         if (myNonFriends.length > 0) {
             return (
@@ -66,15 +69,7 @@ export default function NonFriendList(props) {
                         <div className="friend" key={nonFriend.id} >
                             <Row>
                                 <Col>
-                                    <h3 className="friend_username"> {nonFriend.username} </h3>
-                                </Col>
-                                <Col>
-                                    <Button onClick={(e) => postFriendRequest(nonFriend.id)}>
-                                        <p> Follow </p> 
-                                    </Button>   
-                                    <Button onClick={(e) => cancelFriendRequest(nonFriend.id)}>
-                                        <p> X </p> 
-                                    </Button>
+                                    <h3 className="friend_username"> {nonFriend.recommended_user} </h3>
                                 </Col>
                             </Row>
                         </div>
@@ -82,10 +77,40 @@ export default function NonFriendList(props) {
                 })
             )
         } else {
-            return (<h3> You don't have any friend connections yet. </h3>)
+            return (<h3> You don't have any friend suggestions yet. </h3>)
 
         }
     }
+
+    // const displayNonFriends = (e) => {
+    //     if (myNonFriends.length > 0) {
+    //         return (
+    //             myNonFriends.map((nonFriend, index) => {
+    //                 console.log(nonFriend);
+    //                 return (
+    //                     <div className="friend" key={nonFriend.id} >
+    //                         <Row>
+    //                             <Col>
+    //                                 <h3 className="friend_username"> {nonFriend.username} </h3>
+    //                             </Col>
+    //                             <Col>
+    //                                 <Button onClick={(e) => postFriendRequest(nonFriend.id)}>
+    //                                     <p> Follow </p>
+    //                                 </Button>
+    //                                 <Button onClick={(e) => cancelFriendRequest(nonFriend.id)}>
+    //                                     <p> X </p>
+    //                                 </Button>
+    //                             </Col>
+    //                         </Row>
+    //                     </div>
+    //                 )
+    //             })
+    //         )
+    //     } else {
+    //         return (<h3> You don't have any friend connections yet. </h3>)
+    //
+    //     }
+    // }
 
     return (
         <>
