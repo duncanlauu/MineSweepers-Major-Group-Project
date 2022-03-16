c3ff6c24e2faed1133bfab8005b67facec40182e
Patrik Horsky <patrik.horsky@kcl.ac.uk>
2022-03-16 14:23:24 +0000

    Change staticfiles

--- bookclub/settings.py
+++ bookclub/settings.py
@@ -140,11 +140,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'frontend/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
