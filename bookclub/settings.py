5ddcde3aac1a9a1f3629e1d84de9c925e38297c6
Patrik Horsky <patrik.horsky@kcl.ac.uk>
2022-03-16 14:28:13 +0000

    Change static file path

--- bookclub/settings.py
+++ bookclub/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static/frontend'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
