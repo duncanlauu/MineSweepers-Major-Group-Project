57675b66ecfacf42fa89f31fe2631c74982830db
Patrik Horsky <patrik.horsky@kcl.ac.uk>
2022-03-17 12:22:23 +0000

    Change order of import statements in asgi

--- bookclub/asgi.py
+++ bookclub/asgi.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os, django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookclub.settings')
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.routing import get_default_application
 from django.core.asgi import get_asgi_application
 import app.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookclub.settings')
-django.setup()
+
 
 # Messaging implementation from: https://channels.readthedocs.io tutorial
 application = ProtocolTypeRouter({
