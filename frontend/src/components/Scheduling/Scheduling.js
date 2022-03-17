ac8716b6f202feb50ef74e22aba797168f99eeb6
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-03-17 16:59:28 +0000

    Add the recommended books

--- frontend/src/components/Scheduling/Scheduling.js
+++ frontend/src/components/Scheduling/Scheduling.js
@@ -1,5 +1,4 @@
-import React, {Component, useState, useEffect} from "react";
-import {useNavigate} from "react-router";
+import React, {useState, useEffect} from "react";
 import axiosInstance from "../../axios";
 import {Button, Col, Container, FormGroup, Input, Label, Row} from "reactstrap";
 import {FormLayout, HeadingText, ParaText, SignUpContainer} from "../CreateClub/CreateClubElements";
@@ -8,11 +7,34 @@ import useGetUser from "../../helpers";
 
 export default function Scheduling() {
     const user = useGetUser();
-    let book = [];
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        getRecommendedBooks();
+    }, []);
+
+    const getRecommendedBooks = () => {
+        axiosInstance
+            .post('recommender/0/10/3/top_n_for_club/', {})
+            .then(() => {
+                axiosInstance
+                    .get('recommender/0/10/3/top_n_for_club/', {})
+                    .then((res) => {
+                        let book_list = []
+                        for (let i = 0; i < res.data.length; ++i) {
+                            book_list.push({id: res.data[i]['book'], name: res.data[i]['book']})
+                        }
+                        setBooks(book_list)
+                        console.log(book_list)
+                        console.log(books)
+                    })
+            })
+    }
 
     const initialFormData = Object.freeze({
         name: '',
         description: '',
+        book: '',
         start_time: '',
         end_time: '',
         link: ''
@@ -21,6 +43,7 @@ export default function Scheduling() {
     const [formData, updateFormData] = useState(initialFormData)
 
     const handleChange = (e) => {
+        console.log("changes")
         updateFormData({
             ...formData, // ... is spread syntax. Slits the iterable into individual elements
             [e.target.name]: e.target.value.trim(), // Referring to the forms elements name attribute. Trimming whitespace
@@ -31,32 +54,21 @@ export default function Scheduling() {
         e.preventDefault()
         console.log("submitting", formData)
 
+
         axiosInstance
-            .post('recommender/0/10/3/top_n_for_club/', {})
+            .post(`scheduling/`, {
+                name: formData.name,
+                description: formData.description,
+                club: 3,
+                organiser: user.id,
+                attendees: [2, 8, 110],
+                book: formData.book,
+                start_time: formData.start_time,
+                end_time: formData.end_time,
+                link: formData.link
+            })
             .then((res) => {
-                axiosInstance
-                    .get('recommender/0/10/3/top_n_for_club/', {})
-                    .then((res) => {
-                        for (let i = 0; i < res.data.length; ++i) {
-                            book.append(res.data[i]['book'])
-                        }
-                        axiosInstance
-                            .post(`scheduling/`, {
-                                name: formData.name,
-                                description: formData.description,
-                                club: 3,
-                                organiser: user.id,
-                                attendees: [2, 8, 110],
-                                book: book[0],
-                                start_time: formData.start_time,
-                                end_time: formData.end_time,
-                                link: formData.link
-                            })
-                            .then((res) => {
-                                console.log(res)
-                                console.log(res.data)
-                            })
-                    })
+                console.log(res.data)
             })
 
 
@@ -70,99 +82,114 @@ export default function Scheduling() {
 
     }
 
-    return (
-        <div id="ParentDiv">
-            <Row>
-            </Row>
-            <Container fluid>
-                <Row style={{marginTop: "6rem"}}>
-                    <Col/>
-                    <Col>
-                        <HeadingText>Create a meeting</HeadingText>
-                        <ParaText/>
-                        <SignUpContainer>
-                            <FormLayout>
-                                <FormGroup>
-                                    <Label for="name">Name </Label>
-                                    <Input
-                                        id="name"
-                                        name="name"
-                                        onChange={handleChange}
-                                        style={{border: "0", backgroundColor: "#F3F3F3"}}
-                                    />
-                                </FormGroup>
+    let booksList = books.length > 0
+        && books.map((item, i) => {
+            return (
+                <option key={i} value={item.id}>{item.name}</option>
+            )
+        }, this);
 
-                                <FormGroup>
-                                    <Label for="description"> Description </Label>
-                                    <Input
-                                        id="description"
-                                        name="description"
-                                        onChange={handleChange}
-                                        style={{border: "0", backgroundColor: "#F3F3F3"}}
-                                    />
-                                </FormGroup>
-
-                                <FormGroup>
-                                    <Label for="book"> Book </Label>
-                                    <select value={this.state.value} onChange={handleChange} style={{border: "0", backgroundColor: "#F3F3F3"}}>
-                                        <option value={book[0]}>book[0]</option>
-                                        <option value={book[1]}>book[1]</option>
-                                    </select>
-                                </FormGroup>
-
-                                <FormGroup>
-                                    <Label for="start_time"> Start time </Label>
-                                    <Input
-                                        id="start_time"
-                                        name="start_time"
-                                        type="datetime-local"
-                                        onChange={handleChange}
-                                        style={{border: "0", backgroundColor: "#F3F3F3"}}
-                                    />
-                                </FormGroup>
-
-                                <FormGroup>
-                                    <Label for="end_time"> End time </Label>
-                                    <Input
-                                        id="end_time"
-                                        name="end_time"
-                                        type="datetime-local"
-                                        onChange={handleChange}
-                                        style={{border: "0", backgroundColor: "#F3F3F3"}}
-                                    />
-                                </FormGroup>
-
-                                <FormGroup>
-                                    <Label for="link"> Meeting link </Label>
-                                    <Input
-                                        id="link"
-                                        name="link"
-                                        onChange={handleChange}
-                                        style={{border: "0", backgroundColor: "#F3F3F3"}}
-                                    />
-                                </FormGroup>
-
-
-                                <FormGroup>
-                                    <Col sm={{size: 10, offset: 5}}>
-                                        <Button
-                                            type="submit"
-                                            className="submit"
-                                            onClick={handleSubmit}
-                                            style={{backgroundColor: "#653FFD", width: "7rem"}}
-                                        >
-                                            Create
-                                        </Button>
-                                    </Col>
-                                </FormGroup>
-
-                            </FormLayout>
-                        </SignUpContainer>
-
-                    </Col>
-                    <Col/>
+    if (books.length > 0) {
+        return (
+            <div id="ParentDiv">
+                <Row>
                 </Row>
-            </Container>
-        </div>
-    );
+                <Container fluid>
+                    <Row style={{marginTop: "6rem"}}>
+                        <Col/>
+                        <Col>
+                            <HeadingText>Create a meeting</HeadingText>
+                            <ParaText/>
+                            <SignUpContainer>
+                                <FormLayout>
+                                    <FormGroup>
+                                        <Label for="name">Name </Label>
+                                        <Input
+                                            id="name"
+                                            name="name"
+                                            onChange={handleChange}
+                                            style={{border: "0", backgroundColor: "#F3F3F3"}}
+                                        />
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <Label for="description"> Description </Label>
+                                        <Input
+                                            id="description"
+                                            name="description"
+                                            onChange={handleChange}
+                                            style={{border: "0", backgroundColor: "#F3F3F3"}}
+                                        />
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <Label for="book"> Book </Label>
+                                        <select
+                                            onChange={handleChange}
+                                            style={{border: "0", backgroundColor: "#F3F3F3"}}
+                                        >
+                                            {booksList}
+                                        </select>
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <Label for="start_time"> Start time </Label>
+                                        <Input
+                                            id="start_time"
+                                            name="start_time"
+                                            type="datetime-local"
+                                            onChange={handleChange}
+                                            style={{border: "0", backgroundColor: "#F3F3F3"}}
+                                        />
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <Label for="end_time"> End time </Label>
+                                        <Input
+                                            id="end_time"
+                                            name="end_time"
+                                            type="datetime-local"
+                                            onChange={handleChange}
+                                            style={{border: "0", backgroundColor: "#F3F3F3"}}
+                                        />
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <Label for="link"> Meeting link </Label>
+                                        <Input
+                                            id="link"
+                                            name="link"
+                                            onChange={handleChange}
+                                            style={{border: "0", backgroundColor: "#F3F3F3"}}
+                                        />
+                                    </FormGroup>
+
+
+                                    <FormGroup>
+                                        <Col sm={{size: 10, offset: 5}}>
+                                            <Button
+                                                type="submit"
+                                                className="submit"
+                                                onClick={handleSubmit}
+                                                style={{backgroundColor: "#653FFD", width: "7rem"}}
+                                            >
+                                                Create
+                                            </Button>
+                                        </Col>
+                                    </FormGroup>
+
+                                </FormLayout>
+                            </SignUpContainer>
+
+                        </Col>
+                        <Col/>
+                    </Row>
+                </Container>
+            </div>
+        );
+    } else {
+        return (
+            <div>Wait please</div>
+        )
+    }
 }
