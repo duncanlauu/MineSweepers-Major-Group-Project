e4070f6aad6a25a9f3c4e9ff55fcf11e60322428
Valentin <valentin.magis@hotmail.de>
2022-03-18 10:53:41 +0000

    Refine AuthProvider to use localstorage as default state

--- frontend/src/components/RequireAuth/RequireAuth.js
+++ frontend/src/components/RequireAuth/RequireAuth.js
@@ -1,21 +1,10 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import React, { useEffect } from "react";
+import React from "react";
 
 const RequireAuth = () => {
     const { auth } = useAuth();
-    // const { setAuth } = useAuth();
     const location = useLocation();
-
-    // useEffect(() => {
-    //     const user = localStorage.getItem('username')
-    //     console.log("Loading in require auth: ", user)
-    //     setAuth({ "user": user }) // this has no effect when switching pages. 
-    //     console.log("Auth before: ", auth)
-    //     console.log("Auth user before: ", auth.user)
-    // }, [])
-
-    // console.log("Auth user: ", auth.user)
     return (
         auth?.user
             ? <Outlet />
--- frontend/src/components/context/AuthProvider.js
+++ frontend/src/components/context/AuthProvider.js
@@ -1,9 +1,9 @@
 import React, { createContext, useEffect, useState } from "react";
 
-const AuthContext = createContext({ user: "none" });
+const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => { // children are the components nested inside the auth provider
-    const [auth, setAuth] = useState({ user: "none" });
+    const [auth, setAuth] = useState({ user: localStorage.getItem('username') });
 
     useEffect(() => {
         const user = localStorage.getItem('username')
