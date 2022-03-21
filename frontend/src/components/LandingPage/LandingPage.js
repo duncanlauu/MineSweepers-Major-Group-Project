2dc926c8ca4322d4e898d3aadba676b973fc21a7
Valentin <valentin.magis@hotmail.de>
2022-03-21 21:34:27 +0000

    Add snapshot test for landing page

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {Container, Row, Col} from 'reactstrap'
-import {LogoText, RoundedButton, WelcomeText} from './LandingPageElements'
+import { Link } from 'react-router-dom'
+import { Container, Row, Col } from 'reactstrap'
+import { LogoText, RoundedButton, WelcomeText } from './LandingPageElements'
 
 const LandingPage = () => {
     return (
         <div>
             <Container fluid>
-                <Row style={{marginTop: "10rem"}}>
-                    <Col/>
+                <Row style={{ marginTop: "10rem" }}>
+                    <Col />
                     <Col>
-                        <WelcomeText>Welcome to</WelcomeText><br/>
-                        <LogoText>bookgle</LogoText><br/>
-                        <Link to="/log_in">
-                            <RoundedButton>LOG IN</RoundedButton><br/>
+                        <WelcomeText>Welcome to</WelcomeText><br />
+                        <LogoText>bookgle</LogoText><br />
+                        <Link to="/log_in" data-testid="login_link">
+                            <RoundedButton>LOG IN</RoundedButton><br />
                         </Link>
                         <Link to="/sign_up">
                             <RoundedButton>SIGN UP</RoundedButton>
--- frontend/tests/LandingPage/landingpage.test.js
+++ frontend/tests/LandingPage/landingpage.test.js
@@ -2,14 +2,19 @@ import React from "react"
 import ReactDOM from "react-dom"
 import LandingPage from "../../src/components/LandingPage/LandingPage"
 import renderer from "react-test-renderer"
-import { render, cleanup } from "@testing-library/react"
+import { render, screen, cleanup } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 
 afterEach(cleanup)
 
 it("renders without crashing", () => {
-    // const div = document.createElement("div")
     render(<LandingPage />, { wrapper: MemoryRouter })
+})
+
+it("displays the required links", () => {
+    render(<LandingPage />, { wrapper: MemoryRouter })
+    const login_link = screen.getByTestId('login_link')
+
 })
 
 it("matches snapshot", () => {
