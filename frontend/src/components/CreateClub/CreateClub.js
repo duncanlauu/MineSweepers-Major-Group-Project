6f1170399c1b4895c78aa7d594d28a14387cd36c
Simon Virag <simon.virag@kcl.ac.uk>
2022-03-21 19:34:32 +0000

    Remove coverage and empty SignUp test

--- frontend/src/components/CreateClub/CreateClub.js
+++ frontend/src/components/CreateClub/CreateClub.js
@@ -12,92 +12,6 @@ export default function CreateClub() {
         visibility: true,
         public: true,
     })
-<<<<<<< HEAD
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("submitting", formData)
-
-    axiosInstance
-      .post(`clubs/`, { 
-        name: formData.name,
-        description: formData.description,
-        visibility: formData.visibility,
-        public: formData.public
-      })
-      .then((res) => {
-        navigate("/home/") 
-        console.log(res)
-        console.log(res.data)
-      })
-  }
-
-  // Todo: move styles to a CSS file?
-  return (
-
-
-    <div id="ParentDiv">
-
-      <Row>
-        
-      </Row>
-
-
-      <Container fluid>
-        <Row style={{ marginTop: "6rem" }}>
-          <Col />
-          <Col>
-            <HeadingText>Create a Club</HeadingText>
-            <ParaText></ParaText>
-            <SignUpContainer>
-              <FormLayout> {/*  might have to add more info here */}
-                <FormGroup>
-                  <Label for="name">Name </Label>
-                  <Input
-                    data-testid="name"
-                    id="name"
-                    name="name"
-                    onChange={handleChange}
-                    style={{ border: "0", backgroundColor: "#F3F3F3" }}
-                  />
-                </FormGroup>
-
-                <FormGroup>
-                  <Label for="description"> description </Label>
-                  <Input
-                    data-testid="description"
-                    id="description"
-                    name="description"
-                    onChange={handleChange}
-                    style={{ border: "0", backgroundColor: "#F3F3F3" }}
-                  />
-                </FormGroup>
-
-                
-                <FormGroup>
-                  <Col sm={{ size: 10, offset: 5 }}>
-                    <Button
-                      type="submit"
-                      className="submit"
-                      onClick={handleSubmit}
-                      style={{ backgroundColor: "#653FFD", width: "7rem" }}
-                    >
-                      Create
-                    </Button>
-                  </Col>
-                </FormGroup>
-
-              </FormLayout>
-            </SignUpContainer>
-
-          </Col>
-          <Col />
-        </Row>
-      </Container>
-    </div>
-  );
-=======
 
     const [formData, updateFormData] = useState(initialFormData)
 
@@ -188,5 +102,4 @@ export default function CreateClub() {
             </Container>
         </div>
     );
->>>>>>> 4ed477c9a80d97123742cbe698cdd0466ffe4322
 }
