cf2ba77852b7af1f4946a7ed9d9a822a6cb229ab
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-03-28 16:54:11 +0100

    Fix the axios mocking

--- frontend/src/__mocks__/axios.js
+++ frontend/src/__mocks__/axios.js
@@ -1,22 +1,58 @@
 export default {
-    get: jest.fn().mockImplementation((url) => {
-        switch (url) {
-            case "http://127.0.0.1:8000/api/book_profile/0195153448":
-                return Promise.resolve({
-                    data: {
-                        "id": "0195153448",
-                        "title": "Classical Mythology",
-                        "author": "Mark P. O. Morford",
-                        "publication_date": "2002",
-                        "publisher": "Oxford University Press",
-                        "image_links_large": "http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg",
-                        "image_links_medium": "http://images.amazon.com/images/P/0195153448.01.MZZZZZZZ.jpg",
-                        "image_links_small": "http://images.amazon.com/images/P/0195153448.01.THUMBZZZ.jpg",
-                        "genre": "Social Science"
+    defaults: {
+        headers: {
+            common: {
+                "Content-Type": "",
+                "Authorization": ""
+            }
+        }
+    },
+    get: jest.fn(() => Promise.resolve({data: {}})),
+    post: jest.fn(() => Promise.resolve({data: {}})),
+    put: jest.fn(() => Promise.resolve({data: {}})),
+    delete: jest.fn(() => Promise.resolve({data: {}})),
+    create: jest.fn(function () {
+        return {
+            interceptors: {
+                request: {
+                    use: jest.fn(() => Promise.resolve({data: {}})),
+                },
+                response: {
+                    use: jest.fn(() => Promise.resolve({data: {}})),
+                }
+            },
+
+            defaults: {
+                headers: {
+                    common: {
+                        "Content-Type": "",
+                        "Authorization": ""
                     }
-                });
-            default:
-                throw new Error(`UNMATCHED URL: ${url}`);
+                }
+            },
+            get: jest.fn((url) => {
+                switch (url) {
+                    case "books/0195153448":
+                        return Promise.resolve({
+                            data: {
+                                "id": "0195153448",
+                                "title": "Classical Mythology",
+                                "author": "Mark P. O. Morford",
+                                "publication_date": "2002",
+                                "publisher": "Oxford University Press",
+                                "image_links_large": "http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg",
+                                "image_links_medium": "http://images.amazon.com/images/P/0195153448.01.MZZZZZZZ.jpg",
+                                "image_links_small": "http://images.amazon.com/images/P/0195153448.01.THUMBZZZ.jpg",
+                                "genre": "Social Science"
+                            }
+                        });
+                    default:
+                        throw new Error(`UNMATCHED URL: ${url}`);
+                }
+            }),
+            post: jest.fn(() => Promise.resolve({data: {}})),
+            put: jest.fn(() => Promise.resolve({data: {}})),
+            delete: jest.fn(() => Promise.resolve({data: {}})),
         }
     }),
 };
