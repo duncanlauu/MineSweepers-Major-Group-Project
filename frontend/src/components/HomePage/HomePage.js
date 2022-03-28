d94732788ce2f952ac50a418288a12d15e60752d
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-03-28 10:39:58 +0100

    Basic precomputations

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import axiosInstance from '../../axios'
-import { Container, Col, Row } from 'reactstrap'
+import {Container, Col, Row} from 'reactstrap'
 import {
     ClubListItem,
     Heading2Text,
@@ -11,9 +11,9 @@ import {
 } from './HomePageElements'
 import Gravatar from 'react-gravatar'
 import Nav from '../Nav/Nav'
-import { Link } from 'react-router-dom'
-import { ParaText } from './HomePageElements'
-import { IoIosArrowForward } from 'react-icons/io'
+import {Link} from 'react-router-dom'
+import {ParaText} from './HomePageElements'
+import {IoIosArrowForward} from 'react-icons/io'
 
 import useGetUser from "../../helpers";
 import FeedPostList from '../FriendsPage/FeedPostList'
@@ -22,18 +22,27 @@ const HomePage = () => {
     const currentUser = useGetUser();
     console.log("Buenos Dias Home")
     console.log("Current user logged in: " + currentUser.id)
-    
+
     let calculatedRecommendations = false;
     useEffect(() => {
-        if (typeof(currentUser.id) != "undefined" && calculatedRecommendations == false) {
+        if (typeof (currentUser.id) != "undefined" && !calculatedRecommendations) {
             calculatedRecommendations = true;
-            console.log("Calculated Recommend")
             console.log("Now who is logged in ", currentUser.id)
-            postRecommenderCalculation();
+            axiosInstance
+                .get(`recommender/0/10/${currentUser.id}/top_n_clubs_top_club_books/`)
+                .then(res => {
+                    if (res.data.length === 0) {
+                        console.log("Calculated Recommend")
+                        precomputeRecommenderResults();
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                })
         }
     }, [currentUser])
 
-    const postRecommenderCalculation = () => {
+    const precomputeRecommenderResults = () => {
         axiosInstance
             .post(`recommender/0/20/${currentUser.id}/top_n_users_random_books/`)
             .then((res) => {
@@ -42,51 +51,75 @@ const HomePage = () => {
                 console.log(res)
                 // navigate("/friends_page")
             })
+        axiosInstance
+            .post(`recommender/0/10/${currentUser.id}/top_n/`, {})
+            .then(res => {
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        axiosInstance
+            .post(`recommender/0/10/top_n_global/`, {})
+            .then(res => {
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        axiosInstance
+            .post(`recommender/0/10/${currentUser.id}/top_n_clubs_top_club_books/`, {})
+            .then(res => {
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
-    
+
     return (
         <Container fluid>
-            <Row style={{ marginBottom: "3rem" }}>
-                <Nav />
+            <Row style={{marginBottom: "3rem"}}>
+                <Nav/>
             </Row>
             <Row>
-                <Col />
+                <Col/>
                 <Col xs={5}>
                     <FeedContainer>
                         <FeedPostList/>
                     </FeedContainer>
                 </Col>
                 <Col xs={4}>
-                    <HeadingText>Our Recommendations</HeadingText><br />
-                    <Heading2Text>Books</Heading2Text><br />
-                    <Link to="/recommendations/" style={{ color: '#653FFD', textDecoration: 'none', fontSize:"15px" }}>
-                        <ParaText><IoIosArrowForward />See all recommendations</ParaText>
+                    <HeadingText>Our Recommendations</HeadingText><br/>
+                    <Heading2Text>Books</Heading2Text><br/>
+                    <Link to="/recommendations/" style={{color: '#653FFD', textDecoration: 'none', fontSize: "15px"}}>
+                        <ParaText><IoIosArrowForward/>See all recommendations</ParaText>
                     </Link>
                     <RecommendationContainer>
-                        <Gravatar email='blah@blah.com' size={65} />
-                        <RecommendationInfo />
+                        <Gravatar email='blah@blah.com' size={65}/>
+                        <RecommendationInfo/>
                     </RecommendationContainer>
                     <RecommendationContainer>
-                        <Gravatar email='blah@blah.com' size={65} />
-                        <RecommendationInfo />
-                    </RecommendationContainer><br />
-                    <Heading2Text>Clubs</Heading2Text><br />
-                    <ul style={{ display: "flex", flexDirection: "row" }}>
+                        <Gravatar email='blah@blah.com' size={65}/>
+                        <RecommendationInfo/>
+                    </RecommendationContainer><br/>
+                    <Heading2Text>Clubs</Heading2Text><br/>
+                    <ul style={{display: "flex", flexDirection: "row"}}>
                         <ClubListItem>
-                            <Gravatar email='blah@blah.com' />
+                            <Gravatar email='blah@blah.com'/>
                             <span>Club 1</span>
                         </ClubListItem>
                         <ClubListItem>
-                            <Gravatar email='blah@blah.com' />
+                            <Gravatar email='blah@blah.com'/>
                             <span>Club 2</span>
                         </ClubListItem>
                         <ClubListItem>
-                            <Gravatar email='blah@blah.com' />
+                            <Gravatar email='blah@blah.com'/>
                             <span>Club 3</span>
                         </ClubListItem>
                     </ul>
                 </Col>
-                <Col />
+                <Col/>
             </Row>
         </Container>
     )
