e205e405a2ae6fdca4d04cad47bf8c0b715cde78
Duncan Lau <k2038819@kcl.ac.uk>
2022-03-31 18:57:27 +0100

    [Account Views]: Add user_sent_request and user_is_friend field in get_user API

--- app/views/account_views.py
+++ app/views/account_views.py
@@ -1,4 +1,4 @@
-from app.models import User
+from app.models import User, FriendRequest
 from ..serializers import RegisterUserSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
@@ -20,9 +20,15 @@ class CreateUser(APIView):
 
     def get(self, request, *args, **kwargs):
         try:
-            user = User.objects.get(pk=kwargs['id'])
-            serializer = RegisterUserSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            user = request.user
+            other_user = User.objects.get(pk=kwargs['id'])
+            serializer = RegisterUserSerializer(other_user)
+            returned_data = serializer.data
+            user_sent_request = FriendRequest.objects.filter(sender=user, receiver=other_user).exists()
+            user_is_friends = other_user in list(user.friends.all())
+            returned_data['user_sent_request'] = user_sent_request
+            returned_data['user_is_friends'] = user_is_friends
+            return Response(returned_data, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -32,5 +38,4 @@ class CreateUser(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
