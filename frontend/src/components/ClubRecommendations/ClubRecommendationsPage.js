e1ec2ef98770d76284b6a2c0d392bd7d4f3945a3
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-04-02 16:21:34 +0100

    Fix the tests

--- frontend/src/components/ClubRecommendations/ClubRecommendationsPage.js
+++ frontend/src/components/ClubRecommendations/ClubRecommendationsPage.js
@@ -67,7 +67,6 @@ const ClubRecommendationPage = () => {
                                 </div>
                                 {clubRecommendations.map(
                                     clubRecommendation => {
-                                        console.log(clubRecommendation)
                                         return (
                                             <SingleClubRecommendation club={clubRecommendation} />
                                         )
--- frontend/src/components/UserProfile/PersonalPosts/__test__/SinglePersonalPost.test.js
+++ frontend/src/components/UserProfile/PersonalPosts/__test__/SinglePersonalPost.test.js
@@ -8,6 +8,20 @@ import '@testing-library/jest-dom'
 import {act} from 'react-dom/test-utils';
 import SinglePersonalPost from "../SinglePersonalPost";
 import routerWrapper from "../../../../test-helpers";
+import fakeLocalStorage from "../../../../fakeLocalStorage";
+import user from "../../../../mocksData/getCurrentUser.json";
+
+
+beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', {
+        value: fakeLocalStorage,
+    });
+});
+
+beforeEach(() => {
+    window.localStorage.clear();
+    window.localStorage.setItem("user", JSON.stringify(user));
+});
 
 
 describe("Components exist", () => {
