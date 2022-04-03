a6674c019d0ece5c19deb9ee64760622e135de72
Yonathan Setiawan <yonathanbali@gmail.com>
2022-04-03 21:40:57 +0800

    merge with develop

--- frontend/src/components/Feed/PostCommentList.js
+++ frontend/src/components/Feed/PostCommentList.js
@@ -48,7 +48,6 @@ export default function PostCommentList(props) {
     document.getElementById(inputAreaID).value = "";
   };
 
-  const displayCommentsUnderPost = () => {
     if (commentsUnderPost.length > 0) {
       return (
         <div>
@@ -72,7 +71,6 @@ export default function PostCommentList(props) {
             </CommentSectionContainer>
           </div>
 
-<<<<<<< HEAD
           <div style={{ display: "flex", justifyContent: "center" }}>
             <Row style={{ marginTop: "1rem" }}>
               <Col xs="9">
@@ -149,105 +147,6 @@ export default function PostCommentList(props) {
           </div>
         </div>
       );
-=======
-    const displayCommentsUnderPost = (e) => {
-        if (commentsUnderPost.length > 0) {
-            // console.log(commentsUnderPost);
-            return (
-                <div>
-                    <div style={{display: "flex", justifyContent: "center"}}>
-                        <CommentSectionContainer>
-                            {commentsUnderPost.map((comment, index) => {
-                                // console.log(comment);
-                                return (
-                                    <div key={comment.id} style={{marginBottom: "1rem"}}>
-                                        <CommentLine>
-                                            <SinglePostComment currentPost={currentPost} comment={comment}
-                                                               updatePageAfterCommentDeletion={updatePageAfterCommentDeletion}
-                                            />
-                                        </CommentLine>
-                                    </div>
-                                )
-                            })}
-                        </CommentSectionContainer>
-                    </div>
-
-                    <div style={{display: "flex", justifyContent: "center"}}>
-                        <Row style={{marginTop: "1rem"}}>
-                            <Col xs="9">
-                                <Input type="textarea" rows="1"
-                                       data-testid="comment-input"
-                                       id={inputAreaID}
-                                       name="myComment"
-                                       placeholder="Leave a comment here..."
-                                       onChange={handleCommentChange}
-                                       style={{
-                                           border: "0",
-                                           backgroundColor: "#F3F3F3",
-                                           borderBottomLeftRadius: "100px",
-                                           borderTopLeftRadius: "100px",
-                                           height: "3rem"
-                                       }}
-                                />
-                            </Col>
-                            <Col xs="3">
-                                <Button onClick={(e) => {
-                                    uploadComment(e, 0);
-                                    clearInputField()
-                                }}
-                                        style={{
-                                            borderBottomRightRadius: "100px",
-                                            borderTopRightRadius: "100px",
-                                            height: "3rem"
-                                        }}
-                                >
-                                    <p> Send </p>
-                                </Button>
-                            </Col>
-                        </Row>
-                    </div>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <div style={{display: "flex", justifyContent: "center"}}>
-                        <Row style={{marginTop: "1rem"}}>
-                            <Col xs="9">
-                                <Input type="textarea" rows="1"
-                                       id="myComment"
-                                       name="myComment"
-                                       placeholder="Leave a comment here..."
-                                       onChange={handleCommentChange}
-                                    // value={""}
-                                       style={{
-                                           border: "0",
-                                           backgroundColor: "#F3F3F3",
-                                           borderBottomLeftRadius: "100px",
-                                           borderTopLeftRadius: "100px",
-                                           height: "3rem"
-                                       }}
-                                />
-                            </Col>
-                            <Col xs="3">
-                                <Button onClick={(e) => uploadComment(e, 0)}
-                                        style={{
-                                            borderBottomRightRadius: "100px",
-                                            borderTopRightRadius: "100px",
-                                            height: "3rem"
-                                        }}
-                                >
-                                    <p> Send </p>
-                                </Button>
-                            </Col>
-                        </Row>
-                    </div>
-                </div>
-            )
-        }
->>>>>>> 89c3cad9f6788597722bad0c368ef4b70587a2e8
     }
-  };
 
-  return <>{displayCommentsUnderPost()}</>;
-}
+    
