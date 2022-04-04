13cdae04670778c91c14f7fee7a5386988419121
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-04-04 09:19:52 +0100

    Add test for editing profile

--- frontend/src/components/UserProfile/UserProfileEditor.js
+++ frontend/src/components/UserProfile/UserProfileEditor.js
@@ -1,132 +1,114 @@
-import React, { useState, useEffect } from "react";
-import {
-  Container,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Row,
-  Col,
-  Button,
-} from "reactstrap";
+import React, {useState} from "react";
+import {Container, Form, FormGroup, Label, Input, Row, Col, Button} from "reactstrap";
 import axiosInstance from "../../axios";
 
-import { useNavigate } from "react-router";
+import {useNavigate} from "react-router";
 
 export default function UserProfileEditor(props) {
-  const [formData, updateFormData] = useState(props.currentUser);
+    const [formData, updateFormData] = useState(props.currentUser);
 
-  const handleChange = (e) => {
-    updateFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+    const handleChange = (e) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-  const getPostCreatorEmail = (author_id) => {
-    axiosInstance
-      .get(`user/get_update/${author_id}/`)
-      .then((res) => {
-        setPosterEmail(res.data.email);
-      })
-      .catch((error) => console.error(error));
-  };
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    const handleEditRequest = (e) => {
+        e.preventDefault();
 
-  const handleEditRequest = (e) => {
-    e.preventDefault();
+        console.log("User profile editor current user: " + props.currentUser.id)
+        console.log(props.currentUser)
 
-    console.log("User profile editor current user: " + props.currentUser.id)
-    console.log(props.currentUser)
+        axiosInstance
+            .put(`user/get_update/${props.currentUser.id}/`, {
+                action: "edit",
+                password: props.currentUser.password,
+                username: props.currentUser.username,
+                email: props.currentUser.email,
+                first_name: props.currentUser.first_name,
+                last_name: props.currentUser.last_name,
+                location: formData.location,
+                bio: formData.bio,
+            })
+            .then((res) => {
+                localStorage.setItem('user', JSON.stringify(res.data));
+                navigate("/home/");
+                navigate("/user_profile/");
+                console.log("New profile content: ");
+                console.log(res);
+            })
+            .catch((e) => {
+                console.log(e.response.data);
+            });
+    };
 
-    axiosInstance
-      .put(`user/get_update/${props.currentUser.id}/`, {
-        action: "edit",
-        password: props.currentUser.password,
-        username: props.currentUser.username,
-        email: props.currentUser.email,
-        first_name: props.currentUser.first_name,
-        last_name: props.currentUser.last_name,
-        location: formData.location,
-        bio: formData.bio,
-      })
-      .then((res) => {
-        localStorage.setItem('user', JSON.stringify(res.data));
-        navigate("/home/");
-        navigate("/user_profile/");
-        console.log("New profile content: ");
-        console.log(res);
-      })
-      .catch((e) => {
-        console.log(e.response.data);
-      });
-  };
+    const displayPersonalPostForm = (e) => {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <div style={{display: "flex", justifyContent: "center"}}>
+                            <h1> Edit Profile </h1>
+                        </div>
 
-  const displayPersonalPostForm = (e) => {
-    return (
-      <Container fluid>
-        <Row>
-          <Col>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <h1> Edit Profile </h1>
-            </div>
+                        <Container>
+                            <Form>
+                                <FormGroup>
+                                    <Label for="bio"> Bio </Label>
+                                    <Input
+                                        type="textarea"
+                                        rows="5"
+                                        id="bio"
+                                        name="bio"
+                                        onChange={handleChange}
+                                        style={{
+                                            border: "0",
+                                            backgroundColor: "#F3F3F3",
+                                            borderRadius: "20px",
+                                        }}
+                                        value={formData.bio}
+                                    />
+                                    <p/>
+                                </FormGroup>
+                                {/* <div>{titleErr}</div> */}
 
-            <Container>
-              <Form>
-                <FormGroup>
-                  <Label for="bio"> Bio </Label>
-                  <Input
-                    type="textarea"
-                    rows="5"
-                    id="bio"
-                    name="bio"
-                    onChange={handleChange}
-                    style={{
-                      border: "0",
-                      backgroundColor: "#F3F3F3",
-                      borderRadius: "20px",
-                    }}
-                    value={formData.bio}
-                  />
-                  <p/>
-                </FormGroup>
-                {/* <div>{titleErr}</div> */}
+                                <FormGroup>
+                                    <Label for="location"> Location </Label>
+                                    <Input
+                                        id="location"
+                                        name="location"
+                                        onChange={handleChange}
+                                        style={{
+                                            border: "0",
+                                            backgroundColor: "#F3F3F3",
+                                            borderRadius: "20px",
+                                        }}
+                                        value={formData.location}
+                                    />
+                                </FormGroup>
+                                {/* <div>{contentErr}</div> */}
 
-                <FormGroup>
-                  <Label for="location"> Location </Label>
-                  <Input
-                    id="location"
-                    name="location"
-                    onChange={handleChange}
-                    style={{
-                      border: "0",
-                      backgroundColor: "#F3F3F3",
-                      borderRadius: "20px",
-                    }}
-                    value={formData.location}
-                  />
-                </FormGroup>
-                {/* <div>{contentErr}</div> */}
-
-                <FormGroup>
-                  <div style={{ display: "flex", justifyContent: "center" }}>
-                    <Button
-                      type="submit"
-                      className="submit"
-                      onClick={handleEditRequest}
-                      style={{ borderRadius: "50px" }}
-                    >
-                      Save changes
-                    </Button>
-                  </div>
-                </FormGroup>
-              </Form>
+                                <FormGroup>
+                                    <div style={{display: "flex", justifyContent: "center"}}>
+                                        <Button
+                                            type="submit"
+                                            className="submit"
+                                            onClick={handleEditRequest}
+                                            style={{borderRadius: "50px"}}
+                                        >
+                                            Save changes
+                                        </Button>
+                                    </div>
+                                </FormGroup>
+                            </Form>
+                        </Container>
+                    </Col>
+                </Row>
             </Container>
-          </Col>
-        </Row>
-      </Container>
-    );
-  };
-  return <>{displayPersonalPostForm(props)}</>;
+        );
+    };
+    return <>{displayPersonalPostForm(props)}</>;
 }
--- frontend/src/components/UserProfile/__test__/UserProfileEditor.test.js
+++ frontend/src/components/UserProfile/__test__/UserProfileEditor.test.js
@@ -10,6 +10,7 @@ import UserProfileEditor from "../UserProfileEditor";
 import fakeLocalStorage from "../../../fakeLocalStorage";
 import user from "../../../mocksData/getCurrentUser.json"
 import routerWrapper from "../../../test-helpers";
+import userEvent from "@testing-library/user-event";
 
 
 beforeAll(() => {
@@ -27,7 +28,17 @@ describe("Components exist", () => {
 
     test("contains header", async () => {
         act(() => {
-            render(routerWrapper(<UserProfileEditor currentUser={1}/>))
+            render(routerWrapper(<UserProfileEditor currentUser={{
+                id: 1,
+                username: "test",
+                email: "test@test.com",
+                bio: "test bio",
+                location: "test location",
+                createdAt: "2020-01-01T00:00:00.000Z",
+                first_name: "test first name",
+                last_name: "test last name",
+                password: "test password"
+            }}/>))
         })
 
         await waitFor(() => {
@@ -37,7 +48,17 @@ describe("Components exist", () => {
 
     test("contains bio", async () => {
         act(() => {
-            render(routerWrapper(<UserProfileEditor currentUser={1}/>))
+            render(routerWrapper(<UserProfileEditor currentUser={{
+                id: 1,
+                username: "test",
+                email: "test@test.com",
+                bio: "test bio",
+                location: "test location",
+                createdAt: "2020-01-01T00:00:00.000Z",
+                first_name: "test first name",
+                last_name: "test last name",
+                password: "test password"
+            }}/>))
         })
 
         await waitFor(() => {
@@ -47,7 +68,17 @@ describe("Components exist", () => {
 
     test("contains location", async () => {
         act(() => {
-            render(routerWrapper(<UserProfileEditor currentUser={1}/>))
+            render(routerWrapper(<UserProfileEditor currentUser={{
+                id: 1,
+                username: "test",
+                email: "test@test.com",
+                bio: "test bio",
+                location: "test location",
+                createdAt: "2020-01-01T00:00:00.000Z",
+                first_name: "test first name",
+                last_name: "test last name",
+                password: "test password"
+            }}/>))
         })
 
         await waitFor(() => {
@@ -57,11 +88,58 @@ describe("Components exist", () => {
 
     test("contains save changes button", async () => {
         act(() => {
-            render(routerWrapper(<UserProfileEditor currentUser={1}/>))
+            render(routerWrapper(<UserProfileEditor currentUser={{
+                id: 1,
+                username: "test",
+                email: "test@test.com",
+                bio: "test bio",
+                location: "test location",
+                createdAt: "2020-01-01T00:00:00.000Z",
+                first_name: "test first name",
+                last_name: "test last name",
+                password: "test password"
+            }}/>))
         })
 
         await waitFor(() => {
             expect(screen.getByText(`Save changes`)).toBeInTheDocument();
+        })
+    })
+
+    test("correctly updates the user profile", async () => {
+        act(() => {
+            render(routerWrapper(<UserProfileEditor currentUser={{
+                id: 1,
+                username: "test",
+                email: "test@test.com",
+                bio: "test bio",
+                location: "test location",
+                createdAt: "2020-01-01T00:00:00.000Z",
+                first_name: "test first name",
+                last_name: "test last name",
+                password: "test password"
+            }}/>))
+        })
+
+        await waitFor(async () => {
+            const bioInput = await screen.findByLabelText(`Bio`);
+            const locationInput = await screen.findByLabelText(`Location`);
+            const saveChangesButton = await screen.findByText(`Save changes`);
+
+            userEvent.clear(bioInput);
+            userEvent.type(bioInput, "New bio")
+            userEvent.clear(locationInput);
+            userEvent.type(locationInput, "New location")
+
+            userEvent.click(saveChangesButton)
+
+            await new Promise(process.nextTick)
+            expect(JSON.parse(window.localStorage.getItem("user")).bio).toBe("New bio")
+            expect(JSON.parse(window.localStorage.getItem("user")).location).toBe("New location")
+            expect(JSON.parse(window.localStorage.getItem("user")).first_name).toBe("test first name")
+            expect(JSON.parse(window.localStorage.getItem("user")).last_name).toBe("test last name")
+            expect(JSON.parse(window.localStorage.getItem("user")).username).toBe("test")
+            expect(JSON.parse(window.localStorage.getItem("user")).password).toBe("test password")
         })
     })
 })
