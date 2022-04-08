056da6448559c4dd883cee2188fcd10372a3814a
Mikolaj Deja <mikolaj.deja@kcl.ac.uk>
2022-04-08 16:49:22 +0200

    Add retrain command

---
+++ app/management/commands/retrain.py
@@ -0,0 +1,42 @@
+import time
+
+from django.core.management import BaseCommand
+from surprise import SVD
+
+from app.models import UserRecommendation, BookRecommendation, ClubRecommendation, BookRecommendationForClub
+from app.recommender_system.file_management import train_model, test_model, dump_trained_model, get_combined_data, \
+    get_dataset_from_dataframe, get_trainset_from_dataset
+
+
+def remove_recommendations():
+    """Remove all previous recommendations from the database"""
+
+    UserRecommendation.objects.all().delete()
+    BookRecommendation.objects.all().delete()
+    ClubRecommendation.objects.all().delete()
+    BookRecommendationForClub.objects.all().delete()
+
+
+def retrain_the_model():
+    """Retrain the model"""
+
+    csv_file_path = 'app/files/BX-Book-Ratings-deployed.csv'
+    dump_file_name = 'app/files/dump_file'
+    dataframe = get_combined_data(csv_file_path)
+    data = get_dataset_from_dataframe(dataframe)
+    trainset = get_trainset_from_dataset(data)
+    algo = SVD()
+    train_model(algo, trainset)
+    pred = test_model(algo, trainset)
+    dump_trained_model(dump_file_name, algo, pred)
+
+
+class Command(BaseCommand):
+    """A class for creating the deployment version of dataset"""
+
+    def handle(self, *args, **options):
+        start = time.time()
+        remove_recommendations()
+        retrain_the_model()
+        end = time.time()
+        print('Time taken to retrain the model: '.format(end - start))
